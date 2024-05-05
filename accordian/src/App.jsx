b24c06705d4c7ad7e3fd09b2563afe8cf2fa48e3
Rd98891 <rishudutt15@gmail.com>
2024-05-05 14:15:49 +0530

    Initialize the project with Tailwind

---
+++ accordian/src/App.jsx
@@ -0,0 +1,17 @@
+
+import ReactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+      <div className='flex justify-center items-center flex-col'>
+          <h1 className="text-3xl font-bold underline">Sali Chutiya aurat</h1>
+          <img src={ReactLogo} alt='react-logo'/>
+      </div>
+  )
+}
+
+export default App
